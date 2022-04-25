ce3a8b046d092f69dc68847e079e181f43a08dd8
fernandocfbf <fernandocfbf@al.insper.edu.br>
2022-04-25 12:18:59 -0300

    feat: adding minimax structure function

--- src/classes/AutoPlayer.py
+++ src/classes/AutoPlayer.py
@@ -13,8 +13,57 @@ class AutoPlayer(Player):
             inside the board
         '''
         isFullColumn = board[0][column] != 0
-        insideBoard = column < 7
+        insideBoard = column < 7 and column >=0
         return insideBoard and not(isFullColumn)  
+    
+    def getOpponentCode(self, playerCode):
+        '''
+        input: player code [int]
+        output: opponent code [int]
+        description: creturn the opponent code [int]
+        '''
+        if playerCode == 1:
+            return 2
+        return 1
+
+    def sucessors(self, playerCode, board):
+        '''
+        input: player code [int], game board [matriz (6 x 7)]
+        output: all possible sucessors states [list]
+        description: checks all possible next states for
+            the game, and return in a list
+        '''
+        res = list()
+        for column in range(0, 7):
+            check = self.isPossibleMove(board, column)
+            if check:
+                res.append(column)
+        return res
+
+    def _min(self, move):
+        return -1
+    
+    def _max(self, move):
+        return 1
+
+    def minimax(self, board, depth):
+        '''
+        input
+        output:
+        description:
+        '''
+        alpha, beta, action = None
+        for move in self.sucessors(board):
+            move_score = self._min(move)
+
+            if alpha == None:
+                alpha = move
+                action = move_score
+
+            elif move_score > alpha:
+                alpha = move
+                action = move_score
+        return action
 
     #@Override
     def name(self):
@@ -27,4 +76,10 @@ class AutoPlayer(Player):
     
     #@Override
     def move(self, playerCode, board):
-        return random.randint(0, 6)
+        isPossible = False
+        while not(isPossible):
+            number = random.randint(0, 6) 
+            isPossible = self.isPossibleMove(board, number)
+        return number
+
+        
